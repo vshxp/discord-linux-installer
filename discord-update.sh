60e7b145c7c22c5e46a4d0bc65571ae63a868820
vshxp <matheus@matfor.com.br>
2024-01-31 00:34:25 -0300

    fix: discord-update alias creation

--- discord-update.sh
+++ discord-update.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
-echo "Downloading last Discord version..."
 
-url="https://discord.com/api/download?platform=linux&format=tar.gz"
-output_file="discord.tar.gz"
+check_discord_update() {
+    if command -v "discord-update" &> /dev/null; then
+        return 0  # Command exists
+    else
+        return 1  # Command does not exist
+    fi
+}
 
-curl -L -o "$output_file" -A "Mozilla/5.0" "$url"
+download_discord() {
+    echo "Downloading latest Discord version..."
+    url="https://discord.com/api/download?platform=linux&format=tar.gz"
+    output_file="discord.tar.gz"
+    curl -L -o "$output_file" -A "Mozilla/5.0" "$url"
+    echo "Download complete. The file is saved as: $output_file"
+}
 
-echo "Download complete. The file is saved as: $output_file"
-echo "Installing latest discord..."
-sudo rm -rf /opt/Discord
-sudo tar -xvzf discord.tar.gz -C /opt >/dev/null 2>&1
-sudo ln -sf /opt/Discord/Discord /usr/bin/discord
+install_discord() {
+    echo "Installing latest discord..."
+    # sudo rm -rf /opt/discord
+    # sudo tar -xvzf discord.tar.gz -C /opt -k >/dev/null 2>&1
+    sudo tar -xvzf discord.tar.gz --strip-components=1 -C /opt/discord -k >/dev/null 2>&1
+    sudo ln -sf /opt/discord/Discord /usr/bin/discord
+}
 
-echo "Creating discord-update alias"
-sudo cp ./discord-update.sh /opt/Discord/discord-update
-sudo ln -sf /opt/Discord/discord-update /usr/bin/discord-update
-
-echo "Creating desktop icon" 
-cat <<EOF > ~/.local/share/applications/discord.desktop
+create_icon() {
+    echo "Creating desktop icon" 
+    cat <<EOF > ~/.local/share/applications/discord.desktop
 [Desktop Entry]
 Name=Discord
 Comment=All-in-one voice and text chat for gamers
 Exec=/usr/bin/discord
-Icon=/opt/Discord/discord.png
+Icon=/opt/discord/discord.png
 Terminal=false
 Type=Application
 Categories=Network;InstantMessaging;Game;
 EOF
-echo ""
+}
+
+cleanup() {
+    echo "Removing unnecessary installation files..."
+    sudo rm discord.tar.gz >/dev/null 2>&1
+}
+
+create_update_alias() {
+    echo "Creating discord-update alias"
+    sudo cp ./discord-update.sh /opt/discord/discord-update
+    sudo ln -sf /opt/discord/discord-update /usr/bin/discord-update
+}
+
+echo "Discord update v2"
+download_discord
+install_discord
+if check_discord_update; then
+    create_update_alias
+    create_icon
+fi
+cleanup
